2263eccf19e486800af5978d58d2d716c4743d16
FrizDark <dmitriy.steshenko@gmail.com>
2022-08-17 11:11:54 +0300

    Fixes

--- Carstore/View/MainWindow.xaml.cs
+++ Carstore/View/MainWindow.xaml.cs
@@ -114,42 +114,49 @@ namespace Carstore
 
         private async void RegisterViewRegisterClick(object sender, RoutedEventArgs e)
         {
-            using (CarstoreDBEntities db = new CarstoreDBEntities())
+            try
             {
-                string email = _registerView.EmailBox.Text;
-                string phone = _registerView.PhoneBox.Text;
-                phone = $"({phone.Substring(0, 3)}){phone.Substring(3, 3)}-{phone.Substring(6, 2)}-{phone.Substring(8, 2)}";
-                bool isUsedEmail = false;
-                bool isUsedPhone = false;
-                await Task.Run(() => isUsedEmail = db.User.FirstOrDefault(user => user.Email == email) != null);
-                await Task.Run(() => isUsedPhone = db.User.FirstOrDefault(user => user.Phone == phone) != null);
-                if (isUsedEmail && isUsedPhone)
+                using (CarstoreDBEntities db = new CarstoreDBEntities())
                 {
-                    _registerView.MessageBlock.Text = Properties.Resources.registerView_EmailAndPhoneAreUsed;
-                    return;
-                } 
-                else if (isUsedEmail)
-                {
-                    _registerView.MessageBlock.Text = Properties.Resources.registerView_EmailIsUsed;
-                    return;
-                } 
-                else if (isUsedPhone)
-                {
-                    _registerView.MessageBlock.Text = Properties.Resources.registerView_PhoneIsUsed;
-                    return;
+                    string email = _registerView.EmailBox.Text;
+                    string phone = _registerView.PhoneBox.Text;
+                    phone = $"({phone.Substring(0, 3)}){phone.Substring(3, 3)}-{phone.Substring(6, 2)}-{phone.Substring(8, 2)}";
+                    bool isUsedEmail = false;
+                    bool isUsedPhone = false;
+                    await Task.Run(() => isUsedEmail = db.User.FirstOrDefault(user => user.Email == email) != null);
+                    await Task.Run(() => isUsedPhone = db.User.FirstOrDefault(user => user.Phone == phone) != null);
+                    if (isUsedEmail && isUsedPhone)
+                    {
+                        _registerView.MessageBlock.Text = Properties.Resources.registerView_EmailAndPhoneAreUsed;
+                        return;
+                    }
+                    else if (isUsedEmail)
+                    {
+                        _registerView.MessageBlock.Text = Properties.Resources.registerView_EmailIsUsed;
+                        return;
+                    }
+                    else if (isUsedPhone)
+                    {
+                        _registerView.MessageBlock.Text = Properties.Resources.registerView_PhoneIsUsed;
+                        return;
+                    }
+                    db.User.Add(new User
+                    {
+                        Firstname = _registerView.FirstnameBox.Text,
+                        Lastname = _registerView.LastnameBox.Text,
+                        Email = _registerView.EmailBox.Text,
+                        Phone = phone,
+                        Password = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(_registerView.PasswordBox.Password)),
+                        TypeId = db.UserType.First(type => type.Name == "userType_User").Id
+                    });
+                    await db.SaveChangesAsync();
                 }
-                db.User.Add(new User
-                {
-                    Firstname = _registerView.FirstnameBox.Text,
-                    Lastname = _registerView.LastnameBox.Text,
-                    Email = _registerView.EmailBox.Text,
-                    Phone = phone,
-                    Password = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(_registerView.PasswordBox.Password)),
-                    TypeId = db.UserType.First(type => type.Name == "userType_User").Id
-                });
-                await db.SaveChangesAsync();
-                RegisterViewBackClick(sender, e);
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            RegisterViewBackClick(sender, e);
         }
 
         private void RegisterViewBackClick(object sender, RoutedEventArgs e)
@@ -161,44 +168,51 @@ namespace Carstore
 
         private async void LoginViewLoginClick(object sender, RoutedEventArgs e)
         {
-            using (CarstoreDBEntities db = new CarstoreDBEntities())
+            try
             {
-                User user = null;
-                string email = _loginView.EmailBox.Text;
-                await Task.Run(() => user = db.User.FirstOrDefault(usr => usr.Email == email));
-                byte[] hash = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(_loginView.PasswordBox.Password));
-
-                if (user != null)
+                using (CarstoreDBEntities db = new CarstoreDBEntities())
                 {
-                    bool buf = true;
-                    for (int i = 0; i < Math.Min(hash.Length, user.Password.Length); i++)
+                    User user = null;
+                    string email = _loginView.EmailBox.Text;
+                    await Task.Run(() => user = db.User.FirstOrDefault(usr => usr.Email == email));
+                    byte[] hash = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(_loginView.PasswordBox.Password));
+
+                    if (user != null)
                     {
-                        if (hash[i] != user.Password[i])
+                        bool buf = true;
+                        for (int i = 0; i < Math.Min(hash.Length, user.Password.Length); i++)
                         {
-                            buf = false;
-                            break;
+                            if (hash[i] != user.Password[i])
+                            {
+                                buf = false;
+                                break;
+                            }
+                        }
+                        if (buf)
+                        {
+                            SelectedUserId = user.Id;
+
+                            if (_loginView.SavePasswordBox.IsChecked == true)
+                            {
+                                Configuration config = ConfigurationManager.OpenExeConfiguration(GetAppPath());
+                                config.AppSettings.Settings.Remove("UserId");
+                                config.AppSettings.Settings.Add("UserId", user.Id.ToString());
+                                config.Save(ConfigurationSaveMode.Minimal);
+                                EncryptConfigSection("appSettings");
+                            }
+
+                            MainGrid.Children.Clear();
+                            _appView = new AppView(AppLogoutClick);
+                            MainGrid.Children.Add(_appView);
+                            return;
                         }
                     }
-                    if (buf)
-                    {
-                        SelectedUserId = user.Id;
-
-                        if (_loginView.SavePasswordBox.IsChecked == true)
-                        {
-                            Configuration config = ConfigurationManager.OpenExeConfiguration(GetAppPath());
-                            config.AppSettings.Settings.Remove("UserId");
-                            config.AppSettings.Settings.Add("UserId", user.Id.ToString());
-                            config.Save(ConfigurationSaveMode.Minimal);
-                            EncryptConfigSection("appSettings");
-                        }
-
-                        MainGrid.Children.Clear();
-                        _appView = new AppView(AppLogoutClick);
-                        MainGrid.Children.Add(_appView);
-                        return;
-                    }
+                    _loginView.MessageBlock.Text = Properties.Resources.loginView_WrongEmailOrPassword;
                 }
-                _loginView.MessageBlock.Text = Properties.Resources.loginView_WrongEmailOrPassword;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
 
